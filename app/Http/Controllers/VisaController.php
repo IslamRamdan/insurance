2d8f8b81b380644add0a8bc2573cc474a310cfc6
Islam Ramdan <121763270+IslamRamdan@users.noreply.github.com>
2026-05-09 14:06:50 +0300

    logo tab

--- app/Http/Controllers/VisaController.php
+++ app/Http/Controllers/VisaController.php
@@ -7,6 +7,7 @@ use App\Services\FawaterkService;
 use App\Models\VisaTransaction;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class VisaController extends Controller
 {
